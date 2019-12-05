d559fab7c21afce8c70ebb32c1c53e2fcd9e95a7
Terry Cox <terry@bootstrap.je>
2019-12-05 15:29:15 +0000

    fix: Try as root
    Signed-off-by: Terry Cox <terry@bootstrap.je>

--- jx-app-fossa.sh
+++ jx-app-fossa.sh
@@ -1,9 +1,11 @@
-mkdir fossa-dl
-curl "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
+env
+ls -la
+sudo mkdir fossa-dl
+sudo curl "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
     grep '"tag_name":' | \
     sed -E 's/.*"v([^"]+)".*/\1/' | \
     xargs -I {} curl -fL 'https://github.com/fossas/fossa-cli/releases/download/v'{}'/fossa-cli_'{}'_linux_amd64.tar.gz' | tar xzvC fossa-dl && \
-mv fossa-dl/fossa /usr/bin && \
+sudo mv fossa-dl/fossa /usr/bin && \
 rm -rf fossa-dl
 if [ ! -f go.mod ]; then
   # Fossa requires a mod file or lock file
