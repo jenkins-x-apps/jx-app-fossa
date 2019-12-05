5716dfe4706aea5bdc6ce425208f0dc5e5285f2d
Terry Cox <terry@bootstrap.je>
2019-12-05 15:52:11 +0000

    fix: Try as root in Dockerfile
    Signed-off-by: Terry Cox <terry@bootstrap.je>

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM gcr.io/jenkinsxio/dev-env-base:0.0.468-go-alpine
+USER root
 COPY ./jx-app-fossa.sh /
 ENTRYPOINT ["/jx-app-fossa.sh"]
--- jx-app-fossa.sh
+++ jx-app-fossa.sh
@@ -1,15 +1,20 @@
+whoami
+pwd
 env
 ls -la
-sudo mkdir fossa-dl
-sudo curl "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
+mkdir fossa-dl
+curl "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
     grep '"tag_name":' | \
     sed -E 's/.*"v([^"]+)".*/\1/' | \
     xargs -I {} curl -fL 'https://github.com/fossas/fossa-cli/releases/download/v'{}'/fossa-cli_'{}'_linux_amd64.tar.gz' | tar xzvC fossa-dl && \
-sudo mv fossa-dl/fossa /usr/bin && \
+mv fossa-dl/fossa /usr/bin && \
 rm -rf fossa-dl
-if [ ! -f go.mod ]; then
-  # Fossa requires a mod file or lock file
-  go mod init
+if [ -f main.go ]; then
+  # This is a Go project
+  if [ ! -f go.mod ]; then
+    # Fossa requires a mod file or lock file
+    go mod init
+  fi
 fi
 fossa init
 export FOSSA_API_KEY=e8f6249ad76777febbcc6f59b43074b0
