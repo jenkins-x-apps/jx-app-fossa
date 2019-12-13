73709d96e02364d1b569da4eec651c0247ec26fb
Terry Cox <terry@bootstrap.je>
2019-12-13 15:48:50 +0000

    feat: Clean output
    Signed-off-by: Terry Cox <terry@bootstrap.je>

--- jx-app-fossa.sh
+++ jx-app-fossa.sh
@@ -23,10 +23,10 @@ fi
 if [[ ${IS_PREVIEW_PIPELINE} == "true" ]] || [[ ${IS_RELEASE_PIPELINE} == "true" ]] ; then
     echo "FOSSA is scanning dependencies..."
     mkdir fossa-dl
-    curl "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
+    curl -sS "https://api.github.com/repos/fossas/fossa-cli/releases/latest" | \
         grep '"tag_name":' | \
         sed -E 's/.*"v([^"]+)".*/\1/' | \
-        xargs -I {} curl -fL 'https://github.com/fossas/fossa-cli/releases/download/v'{}'/fossa-cli_'{}'_linux_amd64.tar.gz' | tar xzvC fossa-dl && \
+        xargs -I {} curl -sSfL 'https://github.com/fossas/fossa-cli/releases/download/v'{}'/fossa-cli_'{}'_linux_amd64.tar.gz' | tar xzvC fossa-dl && \
     mv fossa-dl/fossa /usr/bin && \
     rm -rf fossa-dl
     if [ -f main.go ]; then
